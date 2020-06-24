d825f0a77942ff5617769dff5684043da2c9b1c9
Rafael lima <rafaellrf09@gmail.com>
2020-06-24 16:07:21 -0300

    atualizando página de profile

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import { useSelector } from 'react-redux';
+
+import { Container } from './styles';
 
 function Profile() {
-  return <h1>Profile</h1>;
+  const profile = useSelector(state => state.user.profile)
+
+  function handleSubmit(data) {
+
+  }
+
+  return(
+  <Container>
+    <Form initialData={profile} onSubmit={handleSubmit}>
+      <Input name='name' placeholder='Nome completo' />
+      <Input name='email' placeholder='Seu endereço de e-mail' type='email' />
+      <hr/>
+      <Input name='oldPassword' placeholder='Sua senha atual' type='password' />
+      <Input name='password' placeholder='Nova senha' type='password' />
+      <Input name='confirmPassword' placeholder='Confirmação de senha' type='password' />
+      <button type='submit'>Atualizar Perfil</button>
+    </Form>
+
+    <button type='button'>Sair do GoBarber</button>
+  </Container>
+  );
 }
 
 export default Profile;
