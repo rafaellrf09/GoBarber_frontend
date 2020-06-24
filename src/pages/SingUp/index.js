88feb98c81e76e9dbc348e8edd3205ddeb96365e
Rafael lima <rafaellrf09@gmail.com>
2020-06-24 16:07:35 -0300

    atualizando logo

--- src/pages/SingUp/index.js
+++ src/pages/SingUp/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '~/assets/logo.png';
+import logo from '~/assets/logo.svg';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform'
 import * as Yup from 'yup'
